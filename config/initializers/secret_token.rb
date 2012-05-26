d955856ed6e5ae9b548936f7fe34715992cf28d1
MrSnukke <MrSnukke@emaillime.com>
2012-05-26 10:39:36 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TesTA::Application.config.secret_token = 'd7e3de0598f13ff2c04a3652e8906e0ad742a1ea585fd77a0e38fabcf3673e085b3538250557b68b67a1b4d0d766f1c805593746f5cd634564cc5d998898239e'
